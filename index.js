f2d64b47653ccd18d4630e6e7da6eef369f5dac4
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-02-13 04:09:36 +0100

    feat: add info helper

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const words = require('./words');
 let denieds = ''; // Memory of all denied letters
 
 (async () => {
+  console.info(`Made for the french version of Wordle: https://wordle.louan.me/
+
+How does it works:
+  The program will ask you for valid, misplaced and denied letters.
+  It will remember all the denied letters you've added word after word.
+  If you know the first and third letter of a word you can set it like so:
+  'o_t' == 'o_t__'
+  Underscores will be ignored.
+  The program will output you a list of posible words. Enjoy!
+`);
   while (true) {
     const validLetters = await prompt('Valid letters:\n');
     const misplacedLetters = await prompt('Misplaced letters:\n');
